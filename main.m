6898713d169bbe72086fe381fb8207252433dabe
Victorine pnt <vpenot001@bordeaux-inp.fr>
2022-11-25 16:53:04 +0100

    Signature ok

--- main.m
+++ main.m
@@ -13,4 +13,11 @@ img=double(img);
 [xA,yA,xB,yB,L]=point_longueur();
 
 %Récupération des données sur la longueur
+N=10000;
+[x,y]=recup_point(xA,yA,xB,yB,N);
 
+%Segment
+
+Sign=Signature(x,y,img);
+
+%Signature
